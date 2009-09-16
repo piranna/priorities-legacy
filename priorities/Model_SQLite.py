bf32d892d3e9514f5021e724774402bad805add2
piranna <piranna@dc2ea3a4-9bc2-11de-825e-994487f65616>
2009-09-16 12:37:38 +0000

    First code upload. It seems to have a bug in Eclipse relating to the View_Gtk.glade file having duplicate widget identifiers, but it's fully operaing.

---
+++ priorities/Model_SQLite.py
@@ -0,0 +1,230 @@
+import sqlite3
+
+
+class Model:
+	connection = None
+
+	# Contructor & destructor
+	def __init__(self, db_name):
+		if db_name:
+			self.Connect(db_name)
+
+
+	def __del__(self):
+		self.unconnect()
+
+
+	def Connect(self, db_name):
+		self.unconnect()
+
+		self.connection = sqlite3.connect(db_name)
+
+		self.connection.row_factory = sqlite3.Row
+		self.connection.isolation_level = None
+
+		try:
+			self.connection.executescript('''
+				CREATE TABLE IF NOT EXISTS objectives
+				(
+					id INTEGER NOT NULL,
+					name VARCHAR(32) NOT NULL,
+					quantity FLOAT NOT NULL DEFAULT 0,
+					expiration timestamp DEFAULT NULL,
+					PRIMARY KEY(id)
+				);
+				CREATE TABLE IF NOT EXISTS requeriments
+				(
+					objective INTEGER NOT NULL,
+					requeriment INTEGER NOT NULL,
+					priority INTEGER NOT NULL DEFAULT 0,
+					alternative INTEGER NOT NULL,
+					quantity FLOAT NOT NULL DEFAULT 1,
+					PRIMARY KEY(objective,requeriment,priority)
+				);
+				''')
+		except sqlite3.Error, e:
+			print "Exception on 'Model_SQLite.py':", e.args[0]
+
+
+	def unconnect(self):
+		if self.connection:
+			self.connection.close()
+			self.connection = None
+
+
+	# Access functions
+	def GetObjective(self,objective_id):
+		query = self.connection.execute('''
+			SELECT * FROM objectives
+				WHERE objectives.id == ?
+			''',
+			(objective_id,))
+		query = query.fetchone()
+		if query:
+			return query
+		return None
+
+
+	def DirectDependencies(self, objective_id=None):
+#		sql = '''
+#			SELECT objectives.quantity AS objective_quantity,expiration,
+		sql = '''
+			SELECT objectives.id AS objective_id,name,objectives.quantity AS objective_quantity,expiration,
+					requeriment,priority,alternative,requeriments.quantity AS requeriment_quantity
+			FROM objectives
+				LEFT OUTER JOIN requeriments
+					ON objectives.id=requeriments.objective
+			'''
+
+		if objective_id:
+			sql += "WHERE objectives.id=="+str(objective_id)
+
+		sql += " ORDER BY objective_id,requeriment,priority"
+#		sql += " ORDER BY objective,requeriment,priority"
+
+
+		return self.connection.execute(sql)
+
+
+	def DirectDependents(self, objective_id):
+		return self.connection.execute('''
+			SELECT * FROM requeriments
+			WHERE alternative==?
+			GROUP BY objective
+			''',
+			(str(objective_id),)).fetchall()
+
+
+	def DirectDependents_minQuantity(self,objective_id):
+		query = self.connection.execute('''
+			SELECT MIN(quantity) AS min_quantity FROM requeriments
+			WHERE alternative==?
+			LIMIT 1
+			''',
+			(str(objective_id),))
+		return query.fetchone()['min_quantity']
+
+
+	def GetId(self, name):
+		query = self.connection.execute('''
+			SELECT id FROM objectives
+				WHERE name==?
+			''',
+			(name,))
+		query = query.fetchone()
+		if query:
+			return query['id']
+		return None
+
+
+	def AddObjective(self, name, quantity=None, expiration=None):
+		# Update old objective
+		objective_id = self.GetId(name)
+
+		if objective_id:
+
+			# Increase quantity
+			if quantity!=None:
+				self.connection.execute('''
+					UPDATE objectives
+					SET quantity=?
+					WHERE id=?
+					''',
+					(quantity, objective_id))
+				print "\t\tQuantity:",quantity
+
+			# Update expiration
+			self.connection.execute('''
+				UPDATE objectives
+				SET expiration=?
+				WHERE id=?
+				''',
+				(expiration, objective_id))
+
+			# Return objective id
+			return objective_id
+
+		# Insert new objective
+		if(quantity==None):
+			quantity=0
+
+		cursor = self.connection.cursor()
+		cursor.execute('''
+			INSERT INTO objectives(name,quantity,expiration)
+				VALUES(?,?,?)
+			''',
+			(name,quantity,expiration))
+
+		# Return objective id
+		return cursor.lastrowid
+
+
+	def AddAlternative(self, alternative, objective, requeriment=None, priority=0, quantity=1):
+		# Get first empty requeriment for this objective
+		def AddRequeriment(objective):
+			query = self.connection.execute('''
+				SELECT DISTINCT requeriment FROM requeriments
+				WHERE objective==?
+				ORDER BY requeriment
+				''',
+				(objective,))
+
+			print
+
+			requeriment = 1
+			for row in query:
+				print "dentro",row['requeriment'],requeriment
+				if row['requeriment'] != requeriment:
+					break
+				requeriment += 1
+
+			print "fuera",requeriment
+
+			# Return requeriment id
+			return requeriment
+
+
+		alternative = self.AddObjective(alternative)
+
+		# Search for old requeriment id
+		for row in self.DirectDependencies(objective):
+			if row['alternative']==alternative:
+				requeriment = row['requeriment']
+				break
+
+		# If there's no old requeriment id,
+		# create a new one
+		if not requeriment:
+			requeriment = AddRequeriment(objective)
+
+
+		self.connection.execute('''
+			INSERT INTO requeriments(objective,requeriment,priority,alternative,quantity)
+				VALUES(?,?,?,?,?)
+			''',
+			(objective,requeriment,priority,alternative,quantity))
+
+		print objective,self.GetName(objective),requeriment,priority,alternative,self.GetName(alternative),quantity
+
+		return requeriment
+
+
+	def GetName(self, objective):
+		query = self.connection.execute('''
+			SELECT name FROM objectives
+				WHERE id==?
+			''',
+			(objective,))
+		query = query.fetchone()
+		if query:
+			return query['name']
+		return None
+
+
+	def DelRequeriments(self, objective):
+		objective = self.GetId(objective)
+		return self.connection.execute('''
+			DELETE FROM requeriments
+			WHERE objective==?
+			''',
+			(objective,))
---
+++ priorities/priorities.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+import sys
+
+
+def usage():
+	print("usage:",sys.argv[0],
+					"[(-d|--database) <database path>]",
+					"[(-i|--input) <input file>]",
+					"[-t|--textmode]",
+					"[-T|--test]",
+					"[-A|--askDB]",
+					"[-D|--defaultDB]")
+
+
+def CheckArgs(args):
+	try:
+		import getopt
+		return getopt.getopt(args,
+							"d:i:tTAD",
+							["database=","input=","textmode","test","askDB","defaultDB"])
+
+	except getopt.GetoptError, err:
+		print str(err)
+		usage()
+		sys.exit(2)
+
+
+
+if __name__ == "__main__":
+
+	# Load preferences
+	import preferences
+	preferences = preferences.Load()
+
+	# Set default vars
+	useDefaultDB = preferences['useDefaultDB']
+	db = preferences['database']
+	textmode = False
+	input = None
+
+	# Check arguments
+	opts,args = CheckArgs(sys.argv[1:])
+
+	# Parse arguments
+	for o,a in opts:
+		if o in ("-d", "--database"):
+			db = a
+			useDefaultDB = True
+
+		elif o in ("-i", "--input"):
+			textmode = True
+			input = a
+
+		elif o in ("-t", "--textmode"):
+			textmode = True
+
+		elif o in ("-T", "--teat"):
+			db = ":memory:"
+			useDefaultDB = True
+
+		elif o in ("-A", "--askDB"):
+			useDefaultDB = False
+
+		elif o in ("-D", "--defaultDB"):
+			useDefaultDB = True
+
+	# Load model
+	import Model_SQLite
+	model = Model_SQLite.Model(db)
+
+	# Load controller
+	import Controller
+	controller = Controller.Controller(model)
+
+	# Load view
+	if not textmode:
+#		try:
+			import View_Gtk
+			interface = View_Gtk.View(controller, db,useDefaultDB)
+#		except: #gtk.GtkWarning, e:
+			print
+			print "An error has ocurred loading GTK interface"
+			print "Loading text mode interface"
+			textmode = True
+
+#		import View_Curses
+#		interface = View_Curses.View(controller, db,useDefaultDB)
+
+	if textmode:
+		import View_Text
+		interface = View_Text.View(controller, db,useDefaultDB,input)
