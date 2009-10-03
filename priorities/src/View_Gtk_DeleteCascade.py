31fab00d81bced2e5cdc742033182b0038f68d11
piranna <piranna@dc2ea3a4-9bc2-11de-825e-994487f65616>
2009-10-03 13:53:12 +0000

    Modificar hijos recursivamente

--- priorities/src/View_Gtk_DeleteCascade.py
+++ priorities/src/View_Gtk_DeleteCascade.py
@@ -3,8 +3,6 @@ import gtk
 
 class DeleteCascade:
 	def __init__(self, controller,objective_id):
-		self.__controller = controller
-
 		builder = gtk.Builder()
 		builder.add_from_file("View_Gtk.glade")
 
@@ -18,21 +16,41 @@ class DeleteCascade:
 		deleteCell.connect('toggled', self.__on_deleteCell_toggled, model)
 
 		# Fill model
-		def Append(parent, tree = {}):
+		def Append(parent, tree):
 			for objective_id in tree.keys():
 				Append(model.append(parent,
-									(objective_id,True)),
+									(objective_id, controller.GetName(objective_id), True,False)),
 						tree[objective_id])
 
 		Append(model.append(None,
-							(objective_id,True)),
-				self.__controller.Get_DeleteObjective_Tree(objective_id))
+							(objective_id, controller.GetName(objective_id), True,False)),
+				controller.Get_DeleteObjective_Tree(objective_id))
 
 		treeview.expand_all()
 
 
 	def __on_deleteCell_toggled(self, cell, path,model):
-		# [To-Do] Modify recursively
-		# [To-Do] Add inconsistent
-		model[path][1] = not model[path][1]
+		def SetChildrens(iterator,column, value):
+			iterator = model.iter_children(iterator)
+			while iterator:
+				model.set_value(iterator,2, value)
+				SetChildrens(iterator,column, value)
+				iterator = model.iter_next(iterator)
+
+		def SetAncestorsInconsistency(iterator,column, value):
+			iterator = model.iter_parent(iterator)
+			while iterator:
+#				model.set_value(iterator,2, value)
+				SetAncestors(iterator,column, value)
+				iterator = model.iter_next(iterator)
+
+		iterator = model.get_iter(path)
+		if model.get_value(iterator,2):
+			model.set_value(iterator,2, False)
+			SetChildrens(iterator,2, False)
+#			SetAncestorsInconsistency(iterator,2, False)
+		else:
+			model.set_value(iterator,2, True)
+			SetChildrens(iterator,2, True)
+#			SetAncestorsInconsistency(iterator,2, True)
 
